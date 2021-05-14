bdf899031aeddce4611b59b547c115e6c61e174a
sxenny <60220767+sxenny@users.noreply.github.com>
2021-05-15 00:09:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const db = [];
+
+exports.saveSync = (doc) => {
+    db.push(doc);
+};
+
+exports.first = (obj) => {
+    return db.filter((doc) => {
+        for (let key in obj) {
+            if (doc[key] != obj[key]) {
+                return false;
+            }
+        }
+        return true;
+    }).shift();
+};
+
+exports.clear = () => {
+    db.length = 0;
+};
+
+exports.save = (doc, cb) => {
+    db.push(doc);
+    if (cb) {
+        setTimeout(() => {
+            cb();
+        }, 1000);
+    }
+};
